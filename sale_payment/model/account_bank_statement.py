8b28ac1c66e63ab6a5624545d319c46bed28e749
Florian da Costa <florian.dacosta@akretion.com>
2017-01-31 19:51:30 +0100

    fix flake8

--- sale_payment/model/account_bank_statement.py
+++ sale_payment/model/account_bank_statement.py
@@ -1,23 +1,7 @@
 # -*- coding: utf-8 -*-
-##############################################################################
-#
-#    Author: Florian da Costa
-#    Copyright 2016 Akretion
-#
-#    This program is free software: you can redistribute it and/or modify
-#    it under the terms of the GNU Affero General Public License as
-#    published by the Free Software Foundation, either version 3 of the
-#    License, or (at your option) any later version.
-#
-#    This program is distributed in the hope that it will be useful,
-#    but WITHOUT ANY WARRANTY; without even the implied warranty of
-#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#    GNU Affero General Public License for more details.
-#
-#    You should have received a copy of the GNU Affero General Public License
-#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
-#
-##############################################################################
+# Copyright (C) 2016  Akretion
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
 from openerp import models, fields, api
 
 
@@ -42,4 +26,4 @@ class AccountBankStatementLine(models.Model):
         if self.sale_ids:
             self.partner_id = self.sale_ids[0].partner_id.id
             self.account_id = self.partner_id.\
-                              property_account_receivable_id.id
+                property_account_receivable_id.id
--- sale_payment/model/sale.py
+++ sale_payment/model/sale.py
@@ -1,26 +1,8 @@
 # -*- coding: utf-8 -*-
-#########################################################################
-#  Copyright (C) 2011 Akretion (Sébastien BEAU)                         #
-#  Copyright 2013 Camptocamp SA (Guewen Baconnier)                      #
-#  Copyright (C) 2016  Akretion                                          #
-#                                                                       #
-# This program is free software: you can redistribute it and/or modify  #
-# it under the terms of the GNU Affero General Public License as        #
-# published by the Free Software Foundation, either version 3 of the    #
-# License, or (at your option) any later version.                       #
-#                                                                       #
-# This program is distributed in the hope that it will be useful,       #
-# but WITHOUT ANY WARRANTY; without even the implied warranty of        #
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         #
-# GNU Affero General Public Licensefor more details.                    #
-#                                                                       #
-# You should have received a copy of the                                #
-# GNU Affero General Public License                                     #
-# along with this program.  If not, see <http://www.gnu.org/licenses/>. #
-#                                                                       #
-#########################################################################
+# Copyright (C) 2016  Akretion
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
-from openerp import fields, models, api, _
+from openerp import fields, models, api
 import openerp.addons.decimal_precision as dp
 
 
@@ -29,21 +11,19 @@ class SaleOrder(models.Model):
 
     @api.one
     def _get_residual(self):
-        total = 0.0
         paid_amount = 0.0
         for line in self.payment_ids:
             amount = line.credit - line.debit
             if line.currency_id != self.currency_id:
-                from_currency = (line.currency_id and \
+                from_currency = (line.currency_id and
                                  line.currency_id.with_context(
-                                    date=line.date)) or \
-                                 line.company_id.currency_id.with_context(
-                                    date=line.date)
+                                     date=line.date)) or \
+                    line.company_id.currency_id.with_context(
+                        date=line.date)
                 amount = from_currency.compute(amount, self.currency_id)
             paid_amount += amount
         self.residual = self.amount_total - paid_amount
         self.amount_paid = paid_amount
-            
 
     payment_ids = fields.Many2many(
         comodel_name='account.move.line',
