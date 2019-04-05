6db2d737317f3575c7587a4ab334834f7d2ba4c4
Mourad EL HADJ MIMOUNE <mourad.elhadj.mimoune@akretion.com>
2019-04-05 14:59:11 +0200

    [FIX] add option_ids in  _prepare_add_missing_fields

--- sale_line_option/models/sale.py
+++ sale_line_option/models/sale.py
@@ -93,6 +93,21 @@ class SaleOrderLine(models.Model):
         ]
         return res
 
+    #FIX _prepare_add_missing_fields don't change option_ids values
+    @api.model
+    def _prepare_add_missing_fields(self, values):
+        """ Deduce missing required fields from the onchange """
+        res = super(SaleOrderLine, self)._prepare_add_missing_fields(values)
+        if 'option_ids' not in res:
+            onchange_fields = ['option_ids',]
+            if values.get('order_id') and values.get('product_id') and any(f not in values for f in onchange_fields):
+                line = self.new(values)
+                line.product_id_change()
+                for field in onchange_fields:
+                    if field not in values:
+                        res[field] = line._fields[field].convert_to_write(line[field], line)
+        return res
+
 
 class SaleOrderLineOption(models.Model):
     _name = 'sale.order.line.option'
