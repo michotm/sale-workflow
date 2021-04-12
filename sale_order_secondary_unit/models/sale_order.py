aec06e582840dcf4da49db81ff14506970e24783
Mourad <mourad.elhadj.mimoune@akretion.com>
2021-04-12 20:11:30 +0200

    [IMP] add product_secondary_unit_mixin

--- sale_order_secondary_unit/models/sale_order.py
+++ sale_order_secondary_unit/models/sale_order.py
@@ -1,56 +1,27 @@
 # Copyright 2018-2020 Tecnativa - Carlos Dauden
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
 from odoo import api, fields, models
-from odoo.tools.float_utils import float_compare, float_round
 
 
 class SaleOrderLine(models.Model):
-    _inherit = "sale.order.line"
+    _inherit = ["sale.order.line", "product.secondary.unit.mixin"]
+    _name = "sale.order.line"
 
-    secondary_uom_qty = fields.Float(
-        string="Secondary Qty", digits="Product Unit of Measure"
+    _secondary_unit_fields = {
+        "qty_field": "product_uom_qty",
+        "uom_field": "product_uom",
+    }
+
+    product_uom_qty = fields.Float(
+        store=True, readonly=False, compute="_compute_product_qty", copy=True
     )
-    secondary_uom_id = fields.Many2one(
-        comodel_name="product.secondary.unit",
-        string="Secondary uom",
-        ondelete="restrict",
-    )
 
-    @api.onchange("secondary_uom_id", "secondary_uom_qty")
-    def onchange_secondary_uom(self):
-        if not self.secondary_uom_id:
-            return
-        factor = self.secondary_uom_id.factor * self.product_uom.factor
-        qty = float_round(
-            self.secondary_uom_qty * factor,
-            precision_rounding=self.product_uom.rounding,
-        )
-        if (
-            float_compare(
-                self.product_uom_qty, qty, precision_rounding=self.product_uom.rounding
-            )
-            != 0
-        ):
-            self.product_uom_qty = qty
+    @api.depends("secondary_uom_qty", "secondary_uom_id")
+    def _compute_product_qty(self):
+        self._compute_helper_target_field_qty()
 
-    @api.onchange("product_uom_qty")
-    def onchange_secondary_unit_product_uom_qty(self):
-        if not self.secondary_uom_id:
-            return
-        factor = self.secondary_uom_id.factor * self.product_uom.factor
-        qty = float_round(
-            self.product_uom_qty / (factor or 1.0),
-            precision_rounding=self.secondary_uom_id.uom_id.rounding,
-        )
-        if (
-            float_compare(
-                self.secondary_uom_qty,
-                qty,
-                precision_rounding=self.secondary_uom_id.uom_id.rounding,
-            )
-            != 0
-        ):
-            self.secondary_uom_qty = qty
+    def onchange_product_uom_for_secondary(self):
+        self._onchange_helper_product_uom_for_secondary()
 
     @api.onchange("product_uom")
     def onchange_product_uom_for_secondary(self):
@@ -73,8 +44,7 @@ class SaleOrderLine(models.Model):
 
     @api.onchange("product_id")
     def product_id_change(self):
-        """
-        If default sales secondary unit set on product, put on secondary
+        """If default purchases secondary unit set on product, put on secondary
         quantity 1 for being the default quantity. We override this method,
         that is the one that sets by default 1 on the other quantity with that
         purpose.
@@ -83,5 +53,11 @@ class SaleOrderLine(models.Model):
         self.secondary_uom_id = self.product_id.sale_secondary_uom_id
         if self.secondary_uom_id:
             self.secondary_uom_qty = 1.0
-            self.onchange_secondary_uom()
         return res
+
+    def _onchange_secondary_uom(self):
+        """Only for test purpouse.
+        This PR deletes this method so until the PR is merged
+        https://github.com/OCA/purchase-workflow/pull/1070 we need this alias
+        """
+        self._onchange_helper_product_uom_for_secondary()
