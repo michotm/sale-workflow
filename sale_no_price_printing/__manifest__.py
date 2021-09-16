b648eb9c2d43490b8275379fbeb88b375d3fba21
Florian Mounier <paradoxxx.zero@gmail.com>
2021-09-16 11:05:52 +0200

    [ADD] sale_no_price_printing: Add empty module

---
+++ sale_no_price_printing/__manifest__.py
@@ -0,0 +1,14 @@
+# Copyright 2021 Akretion - Florian Mounier
+{
+    "name": "Sale No Price Printing",
+    "summary": "Allow you to print quotations and order without prices.",
+    "version": "14.0.1.0.0",
+    "author": "Akretion, Odoo Community Association (OCA)",
+    "website": "https://github.com/OCA/sale-workflow",
+    "category": "Sales",
+    "depends": ["sale"],
+    "data": [
+        "views/menus.xml",
+    ],
+    "license": "LGPL-3",
+}
